430814270af4d0b8ee1c99d59116b889b8aa7a96
System10111 <krisi2001@gmail.com>
2021-01-26 17:18:28 +0200

    added player

--- js/main.js
+++ js/main.js
@@ -1,10 +1,97 @@
+// holds all the loaded images
+let imgs = {};
+// the canvas
+let canvas = undefined;
+// context for the canvas
+let ctx = undefined;
+// holds the values for pressed keyboard keys
+let kKeys = {};
+// player data
+let player = {
+    pos: {
+        x:10,
+        y:10
+    }
+}
+
+// function that loads an image
+function loadImage(file)
+{
+    // create an image element
+    let img = document.createElement("img");
+    // set its source
+    img.src = file;
+    // give it back
+    return img;
+}
+
+// initialise the game
 function initialise()
 {
-    var canvas = document.createElement("canvas");
+    // create a canvas
+    canvas = document.createElement("canvas");
     canvas.style = "border:2px solid #000000;"
     canvas.width = 500;
     canvas.height = 500;
+
     document.body.appendChild(canvas);
+
+    // get its context
+    ctx = canvas.getContext("2d");
+    
+    // create an image and add it to the list of loaded images
+    imgs["theFirst"] = loadImage("img/theFirst.png");
 }
 
-window.addEventListener("load", initialise);
+let lastTime = 0;
+function mainLoop(time)
+{
+    // get the elapsed time between frames
+    let deltaT = time - lastTime;
+    // convert from ms to s
+    deltaT = deltaT / 1000;
+    // set the time to be used in the next cycle
+    lastTime = time;
+
+    // UPDATING (we should probably move this code to a separate function later):
+
+    // check pressed keys
+    if (kKeys["W".charCodeAt(0)]) {
+        player.pos.y -= 150 * deltaT;
+    }
+    if (kKeys["S".charCodeAt(0)]) {
+        player.pos.y += 150 * deltaT;
+    }
+    if (kKeys["A".charCodeAt(0)]) {
+        player.pos.x -= 150 * deltaT;
+    }
+    if (kKeys["D".charCodeAt(0)]) {
+        player.pos.x += 150 * deltaT;
+    }
+
+    // DRAWING (we should probably move this code to a separate function later):
+
+    // clear the canvas
+    ctx.save();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+    
+    // draw the image we created
+    ctx.drawImage(imgs["theFirst"], player.pos.x, player.pos.y, 100, 100);
+
+    // OTHER:
+
+    // After everything is done, run the loop again, as soon as we can
+    window.requestAnimationFrame(mainLoop);
+}
+
+// when the window loads, initialise and start the main loop
+window.onload = function() {
+    initialise();
+    mainLoop();
+}
+
+// detect pressed keys
+window.onkeyup = function(e) { kKeys[e.keyCode] = false; }
+window.onkeydown = function(e) { kKeys[e.keyCode] = true; }
